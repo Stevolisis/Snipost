73b88a4b3e054203308ac616db8a6fb15ccebfad
Steven Joseph <66651054+Stevolisis@users.noreply.github.com>
2025-06-05 00:23:09 +0100

    SEO

--- src/app/(sec2)/layout.js
+++ src/app/(sec2)/layout.js
@@ -13,12 +13,32 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "Snipost",
-  description: "Learn, build, share and earn with Snipost",
-  icons: {
-    icon: "/logo.svg",
-    shortcut: "/logo.svg",
-    apple: "/logo.svg",
+  title: 'Snipost',
+  description: 'Where web3 developers share, learn, build and earn with Snipost',
+  openGraph: {
+    title: 'Snipost',
+    description: 'Where web3 developers share, learn, build and earn with Snipost',
+    url: 'https://snipost.vercel.app',
+    siteName: 'Example Blog',
+    images: [
+      {
+        url: 'https://snipost.vercel.app/og-image.png',
+        width: 600,
+        height: 600,
+        alt: 'Snipost - Web3 Developer Community',
+      },
+    ],
+    siteName: "Snipost"
+  },
+  twitter: {
+    card: 'snipost',
+    title: 'Snipost',
+    description: 'Where web3 developers share, learn, build and earn with Snipost',
+    images: ['https://snipost.vercel.app/og-image.png',],
+  },
+  other: {
+    'theme-color': '#E5FF4A',
+    'msapplication-TileColor': '#E5FF4A',
   },
 };
 
