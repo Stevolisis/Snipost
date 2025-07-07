bc3a9871fa9da8dd9d8fef4eac7bae6c9afd85c6
Steven Joseph <66651054+Stevolisis@users.noreply.github.com>
2025-07-07 17:20:12 +0100

    switching to devnet for now

--- src/components/appComponents/WalletContextProvider.js
+++ src/components/appComponents/WalletContextProvider.js
@@ -1,26 +1,18 @@
 "use client";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
+import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { useMemo } from "react";
-import { useWallet } from "@solana/wallet-adapter-react";
 
 const WalletContextProvider = ({ children }) => {
-  const HELIUS_RPC = `https://mainnet.helius-rpc.com/?api-key=${process.env.NEXT_PUBLIC_HELIUS_API_KEY}`;
+  const HELIUS_RPC = `https://devnet.helius-rpc.com/?api-key=${process.env.NEXT_PUBLIC_HELIUS_API_KEY}`;
   const endpoint = useMemo(() => HELIUS_RPC, []);
 
   const wallets = useMemo(() => [
     new PhantomWalletAdapter(),
     new SolflareWalletAdapter(),
   ], []);
-
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect={true}>
@@ -29,5 +21,4 @@ const WalletContextProvider = ({ children }) => {
     </ConnectionProvider>
   );
 };
-
 export default WalletContextProvider;
