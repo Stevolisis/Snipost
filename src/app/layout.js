3cf40e87dffd36ef04718f284bb48960d7eedba4
Steven Joseph <66651054+Stevolisis@users.noreply.github.com>
2025-10-01 15:19:33 +0100

    SEO Fix, using html direct

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,40 +9,46 @@ const geist = Geist({
   subsets: ["latin"],
 });
 
-export const metadata = {
-  title: 'Snipost: Building in Public',
-  description: 'Where web3 developers share, learn, build and earn with Snipost',
-  openGraph: {
-    title: 'Snipost',
-    description: 'Where web3 developers share, learn, build and earn with Snipost',
-    url: 'https://snipost.vercel.app',
-    siteName: 'Snipost Blog',
-    images: [
-      {
-        url: 'https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/4JmubmYDJnFtstwHbaZPev/0c3576832aae5b1a4d98c8c9f98863c3/Vercel_Home_OG.png',
-        width: 600,
-        height: 600,
-        alt: 'Snipost - Web3 Developer Community',
-      },
-    ],
-    siteName: "Snipost"
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Snipost',
-    description: 'Where web3 developers share, learn, build and earn with Snipost',
-    images: ['https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/4JmubmYDJnFtstwHbaZPev/0c3576832aae5b1a4d98c8c9f98863c3/Vercel_Home_OG.png',],
-  },
-  other: {
-    'theme-color': '#E5FF4A',
-    'msapplication-TileColor': '#E5FF4A',
-  },
-};
+// export const metadata = {
+//   title: 'Snipost: Building in Public',
+//   description: 'Where web3 developers share, learn, build and earn with Snipost',
+//   openGraph: {
+//     title: 'Snipost',
+//     description: 'Where web3 developers share, learn, build and earn with Snipost',
+//     url: 'https://snipost.vercel.app',
+//     siteName: 'Snipost Blog',
+//     images: [
+//       {
+//         url: 'https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/4JmubmYDJnFtstwHbaZPev/0c3576832aae5b1a4d98c8c9f98863c3/Vercel_Home_OG.png',
+//         width: 600,
+//         height: 600,
+//         alt: 'Snipost - Web3 Developer Community',
+//       },
+//     ],
+//     siteName: "Snipost"
+//   },
+//   twitter: {
+//     card: 'summary_large_image',
+//     title: 'Snipost',
+//     description: 'Where web3 developers share, learn, build and earn with Snipost',
+//     images: ['https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/4JmubmYDJnFtstwHbaZPev/0c3576832aae5b1a4d98c8c9f98863c3/Vercel_Home_OG.png',],
+//   },
+//   other: {
+//     'theme-color': '#E5FF4A',
+//     'msapplication-TileColor': '#E5FF4A',
+//   },
+// };
 
 
 export default function Layout({ children }) {
   return (
     <html lang="en" className="dark">
+        <head>
+            <meta charset="UTF-8" />
+            <link rel="icon" type="image/svg+xml" href="https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/4JmubmYDJnFtstwHbaZPev/0c3576832aae5b1a4d98c8c9f98863c3/Vercel_Home_OG.png" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Voteronics | Mechatronics Voting Portal</title>
+        </head>
       <body className={`${geist.variable} antialiased`}>
         <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
           <WalletContextProvider>
